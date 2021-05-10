89c4dd3d36f2a431ade9603b1fe5378e9b8d900f
Fabian Batioja <f.batioja@uniandes.edu.co>
2021-05-10 12:48:52 -0500

    Rounting prize agregado y ajustes de estilos y accesibilidad

--- src/app/prize/prize.module.ts
+++ src/app/prize/prize.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrizesListComponent } from './prizes-list/prizes-list.component';
+import {PrizeRoutingModule} from './prize-routing.module';
 
 
 
 @NgModule({
   declarations: [PrizesListComponent],
   exports: [
-    PrizesListComponent
+    PrizesListComponent,
+    PrizeRoutingModule
   ],
   imports: [
     CommonModule
